8dc1ba9c9bc7bed871de08bc77110ece96bee170
Romil <romil.m.kothari@gmail.com>
2023-10-17 19:39:26 -0400

    tests completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.list_word = list(word)
+
+    def match(self, givenlist):
+        new_list = []
+        for word in givenlist:
+            word_brokendown = list(word)
+            if sorted(self.list_word) == sorted(word_brokendown):
+                new_list.append(word)
+        return new_list
+                
+
+        
